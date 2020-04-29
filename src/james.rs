93a02ca62ce314f7ca6713c57a4fa3619b0a1d95
James Munns <james.munns@ferrous-systems.com>
2020-04-30 01:34:39 +0200

    I think the grant interfaces are complete, but a little ugly.
    Good enough for now.

--- src/james.rs
+++ src/james.rs
@@ -123,9 +123,12 @@ pub type PhBytes = [u8; 4];
 impl PayloadHeader {
 
     fn to_bytes(self) -> PhBytes {
+
         [
             self.rssi,
             self.pipe,
+
+            // DO NOT REORDER!
             self.length,
             self.pid_no_ack,
         ]
@@ -149,20 +152,75 @@ impl PayloadHeader {
     }
 
     const fn length_idx() -> usize {
+        // DO NOT CHANGE! HW DEPENDANT
         2
     }
 
     const fn pid_no_ack_idx() -> usize {
+        // DO NOT CHANGE! HW DEPENDANT
         3
     }
 
     const fn header_size() -> usize {
         core::mem::size_of::<PhBytes>()
     }
+
+    const fn dma_payload_offset() -> isize {
+        2
+    }
 }
 
 pub struct PayloadR<N: ArrayLength<u8>> {
     pub grant: FrameGrantR<'static, N>,
+}
+
+impl<N> PayloadR<N>
+where
+    N: ArrayLength<u8>
+{
+    pub fn new(
+        raw_grant: FrameGrantR<'static, N>,
+    ) -> Self {
+        Self {
+            grant: raw_grant
+        }
+    }
+
+    pub fn get_header(&self) -> PayloadHeader {
+        const LEN: usize = PayloadHeader::header_size();
+        let mut bytes = [0u8; LEN];
+        bytes.copy_from_slice(&self.grant[..LEN]);
+        PayloadHeader::from_bytes(&bytes)
+    }
+
+    pub unsafe fn dma_pointer(&mut self) -> *const u8 {
+        self.grant
+            .deref()
+            .as_ptr()
+            .offset(PayloadHeader::dma_payload_offset())
+    }
+
+    pub fn pipe(&self) -> u8 {
+        self.grant[PayloadHeader::pipe_idx()]
+    }
+
+    pub fn release(self) {
+        self.grant.release()
+    }
+}
+
+use core::ops::{Deref, DerefMut};
+
+impl<N> Deref for PayloadR<N>
+where
+    N: ArrayLength<u8>
+{
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        let len = self.grant.len();
+        &self.grant[PayloadHeader::header_size()..]
+    }
 }
 
 pub struct PayloadW<N: ArrayLength<u8>> {
@@ -173,22 +231,59 @@ impl<N> PayloadW<N>
 where
     N: ArrayLength<u8>
 {
-    pub fn new(
+    pub(crate) fn new_from_app(
         mut raw_grant: FrameGrantW<'static, N>,
-        header: PayloadHeader,
+        header: PayloadHeader, // HMMMMMM
     ) -> Self {
-        raw_grant[..4].copy_from_slice(&header.to_bytes());
+        raw_grant[..PayloadHeader::header_size()].copy_from_slice(&header.to_bytes());
         Self {
             grant: raw_grant
         }
     }
 
+    pub(crate) fn new_from_radio(
+        mut raw_grant: FrameGrantW<'static, N>,
+    ) -> Self {
+        Self {
+            grant: raw_grant,
+        }
+    }
+
+    pub(crate) fn update_header(
+        &mut self,
+        header: PayloadHeader
+    ) {
+        self.grant[..PayloadHeader::header_size()].copy_from_slice(&header.to_bytes());
+    }
+
+    pub unsafe fn dma_pointer(&mut self) -> *mut u8 {
+        self.grant
+            .deref_mut()
+            .as_mut_ptr()
+            .offset(PayloadHeader::dma_payload_offset())
+    }
+
     pub fn pipe(&self) -> u8 {
         self.grant[PayloadHeader::pipe_idx()]
     }
-}
 
-use core::ops::{Deref, DerefMut};
+    pub fn payload_len(&self) -> usize {
+        self.grant[PayloadHeader::length_idx()] as usize
+    }
+
+    pub fn commit_all(self) {
+        let payload_len = self.payload_len();
+        self.grant.commit(payload_len + PayloadHeader::header_size())
+    }
+
+    pub fn commit(mut self, used: usize) {
+        let max_payload_len = self.payload_len();
+        let payload_len = core::cmp::min(used, max_payload_len);
+        self.grant[PayloadHeader::length_idx()] = payload_len as u8;
+
+        self.grant.commit(payload_len + PayloadHeader::header_size())
+    }
+}
 
 impl<N> Deref for PayloadW<N>
 where
@@ -198,8 +293,7 @@ where
 
     fn deref(&self) -> &Self::Target {
         let len = self.grant.len();
-        let payload_len = len - 1 - 1 - 1 - 1;
-        &self.grant[2..payload_len + 2]
+        &self.grant[PayloadHeader::header_size()..]
     }
 }
 
@@ -208,9 +302,7 @@ where
     N: ArrayLength<u8>,
 {
     fn deref_mut(&mut self) -> &mut [u8] {
-        let len = self.grant.len();
-        let payload_len = len - 1 - 1 - 1 - 1;
-        &mut self.grant[2..payload_len + 2]
+        &mut self.grant[PayloadHeader::header_size()..]
     }
 }
 
