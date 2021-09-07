c1f5aa2434e8e2f6d99366499aba369fbbcddc9c
David O'Connor <david.alan.oconnor@gmail.com>
2021-09-07 18:55:57 -0400

    Added 52833 support

--- src/peripherals.rs
+++ src/peripherals.rs
@@ -7,6 +7,9 @@ use nrf52810_pac as pac;
 #[cfg(feature = "52832")]
 use nrf52832_pac as pac;
 
+#[cfg(feature = "52833")]
+use nrf52833_pac as pac;
+
 #[cfg(feature = "52840")]
 use nrf52840_pac as pac;
 
